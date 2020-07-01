7acae8c7e5d4235ef75ff4fd666b0d7f72bf214f
web.work.waa <66083976+web-work-waa@users.noreply.github.com>
2020-07-01 17:22:30 +0530

    Create bubbles.js

---
+++ bubbles.js
@@ -0,0 +1,126 @@
+tsParticles.load("tsparticles", {
+  fpsLimit: 360,
+  particles: {
+    number: {
+      value: 0,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#ff0000",
+      animation: {
+        enable: true,
+        speed: 180,
+        sync: true
+      }
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000"
+      },
+      polygon: {
+        nb_sides: 5
+      },
+      image: {
+        src: "https://cdn.matteobruni.it/images/particles/github.svg",
+        width: 100,
+        height: 100
+      }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 3,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 15,
+      random: {
+        enable: true,
+        minimumValue: 5
+      },
+      animation: {
+        enable: true,
+        speed: 5,
+        minimumValue: 5,
+        sync: true,
+        startValue: "min",
+        destroy: "max"
+      }
+    },
+    links: {
+      enable: false
+    },
+    move: {
+      enable: true,
+      speed: 15,
+      direction: "none",
+      random: false,
+      straight: false,
+      outMode: "destroy",
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detectsOn: "window",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "trail"
+      },
+      onclick: {
+        enable: true,
+        mode: "push"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 0.8,
+        speed: 3
+      },
+      repulse: {
+        distance: 200
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      },
+      trail: {
+        delay: 0.005,
+        quantity: 5
+      }
+    }
+  },
+  retina_detect: true,
+  background: {
+    color: "#000000",
+    image: "",
+    position: "50% 50%",
+    repeat: "no-repeat",
+    size: "cover"
+  }
+});
